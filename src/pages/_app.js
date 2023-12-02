2421db6d433c99e2d4a21928dd39016aa7730431
Minn KaungMyat <minnkaungmyat00@gmail.com>
2023-12-02 19:18:13 +0630

    Add gtag.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,34 @@
+import React, { useEffect } from 'react';
 import Theme from '../styles/theme';
 
-export default function App({ Component, pageProps }) {
+const analyticsCode = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-LJHLJ0QQVR');
+`;
+
+const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    // Add Google Analytics script dynamically
+    const script = document.createElement('script');
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-LJHLJ0QQVR';
+    script.async = true;
+
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      const analyticsScript = document.createElement('script');
+      analyticsScript.innerHTML = analyticsCode;
+      document.head.appendChild(analyticsScript);
+    };
+
+    // Clean up the dynamically added script on unmount
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
   return (
     <>
       <Theme>
@@ -8,5 +36,6 @@ export default function App({ Component, pageProps }) {
       </Theme>
     </>
   );
-}
- 
+};
+
+export default App;
